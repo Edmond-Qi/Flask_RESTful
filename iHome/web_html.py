624fde1bd60f8957193b683b95bdad967e9e91d9
Edmond-Qi <baofengqi@foxmail.com>
2018-04-21 23:09:01 +0800

    集成项目的静态页面

---
+++ iHome/web_html.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+from flask import Blueprint,current_app
+
+html = Blueprint('html', __name__)
+
+@html.route('/<file_name>')
+def send_html_file(file_name):
+    # 获取静态页面
+    file_name = 'html/' + file_name
+
+    return current_app.send_static_file(file_name)
