3dcb4048a30e92f34ab9bebe78cfad02a08e0b86
Edmond-Qi <baofengqi@foxmail.com>
2018-04-24 17:41:39 +0800

    实名认证前后端逻辑

--- iHome/api_1_0/profile.py
+++ iHome/api_1_0/profile.py
@@ -7,12 +7,62 @@ from iHome.utils.image_storage import image_storage
 from iHome import db, constants
 from iHome.utils.commons import login_required
 
+
+@api.route('/user/auth', methods=['POST'])
+@login_required
+def set_user_auth():
+    """
+    用户实名认证功能:
+    0. todo: 判断用户是否登录
+    1. 获取提交的真实姓名和身份证号并进行参数校验
+    2. todo: 使用第三方接口
+    3. 设置用户的实名认证信息
+    4. 返回应答
+    """
+    # 1. 获取提交的真实姓名和身份证号并进行参数校验
+    req_dict = request.json
+    real_name = req_dict.get('real_name')
+    id_card = req_dict.get('id_card')
+
+    if not all([real_name, id_card]):
+        return jsonify(errno=RET.PARAMERR, errmsg='参数不完整')
+
+    # 2. todo: 使用第三方接口
+    # 3. 设置用户的实名认证信息
+    user_id = g.user_id
+
+    try:
+        user = User.query.get(user_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='查询用户信息失败')
+
+    if not user:
+        return jsonify(errno=RET.USERERR, errmsg='用户不存在')
+
+    # 判断用户是否已经实名认证
+    if user.real_name and user.id_card:
+        return jsonify(errno=RET.DATAEXIST, errmsg='已经实名认证')
+
+    user.real_name = real_name
+    user.id_card = id_card
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='设置实名信息失败')
+
+    # 4. 返回应答
+    return jsonify(errno=RET.OK, errmsg='实名认证成功')
+
 @api.route('/user/name', methods=['PUT'])
 @login_required
 def set_user_name():
     """
     设置用户的用户名:
-    0. todo: 判断用户是否登录
+    0. 判断用户是否登录
     1. 接收用户名并进行校验
     2. 设置用户的用户名
     3. 返回应答
@@ -66,7 +116,7 @@ def set_user_name():
 def set_user_avatar():
     """
     保存上传用户的头像:
-    0. todo: 判断用户是否登录
+    0. 判断用户是否登录
     1. 获取上传头像文件
     2. 上传头像文件到七牛云系统
     3. 设置用户的头像记录
@@ -117,7 +167,7 @@ def set_user_avatar():
 def get_user_info():
     """
     获取用户的个人信息
-    # 0. todo: 判断用户是否登陆
+    # 0. 判断用户是否登陆
     # 1. 获取当前登陆用户的id
     # 2. 根据id获取用户的信息（如果查不到，说明用户不存在）
     # 3. 组织数据，返回应答
